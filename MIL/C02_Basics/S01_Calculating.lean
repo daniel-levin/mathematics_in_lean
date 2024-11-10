beb75f53a28879442a60a300d5a802621e24c20d
Daniel Levin <daniellevin2607@gmail.com>
2024-11-10 14:03:53 +0200

    more exercises

--- MIL/C02_Basics/S01_Calculating.lean
+++ MIL/C02_Basics/S01_Calculating.lean
@@ -105,10 +105,18 @@ section
 variable (a b c d : ℝ)
 
 example : (a + b) * (c + d) = a * c + a * d + b * c + b * d := by
-  sorry
+  calc
+    _ = (a + b) * c + (a+b) *d := by
+      rw [mul_add]
+    _ = a*c + (b*c + a*d) + b*d := by
+      rw [add_mul, add_mul, ← add_assoc, add_assoc (a*c) (b*c)]
+    _ = a*c + a*d + b*c + b*d := by
+      rw [add_comm (b*c) (a*d), ← add_assoc]
 
 example (a b : ℝ) : (a + b) * (a - b) = a ^ 2 - b ^ 2 := by
-  sorry
+      rw [add_mul, mul_sub, mul_sub, add_sub, mul_comm b a]
+      simp
+      rw [pow_two, pow_two]
 
 #check pow_two a
 #check mul_sub a b c
